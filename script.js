ccd54d5495a6ae58a5816f3de63afbba2e3478d7
Vigamy <andreottiwill@gmail.com>
2024-05-07 07:07:47 -0300

    Commit inicial

---
+++ script.js
@@ -0,0 +1,14 @@
+const presente = document.getElementById("presente")
+presente.addEventListener("click", (e) => {
+    presente.classList.add('ppresente')
+    presente.setAttribute("src", "./assets/presente_aberto.png")
+    setInterval(1000, () => {
+        
+        presente.classList.add('aberto')
+    })
+    setTimeout(() => {
+        window.location.href = "./index.html";
+    }, 3000);
+})
+
+    
